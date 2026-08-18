e91d4100f94b9ce839cf1a797b2f25835fedbae9
Alex <deabenderfer@DESKTOP-JAPQJ39.localdomain>
2026-08-18 12:17:22 -0500

    Initial

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'ok'
+  })
+})
+
+module.exports = app
