793d8c33be577c99bebf1d249bc1bccd328ac1b9
TJF <t.fegurgur13@gmail.com>
2026-08-24 15:12:55 -0500

    Frontend route to profile added under App.jsx and skeleton for profile created

--- frontend/invyte-app/src/App.jsx
+++ frontend/invyte-app/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import InvytePortal from "./pages/InvytePortal";
+import Profile from "./pages/Profile";
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<InvytePortal />} />
+				<Route path="/:UserId/profile" element={<Profile />} />
 			</Routes>
 		</>
 	);
---
+++ frontend/invyte-app/src/pages/Profile.jsx
@@ -0,0 +1,55 @@
+import { Link } from 'react-router-dom';
+
+export default function Profile(){
+
+  const user = {
+    rank: 'A1C',
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    phone: '123-456-7890'
+  };
+
+  return(
+    <>
+    <div className="min-h-screen bg-base-200">
+
+      <div className="navbar bg-base-100">
+
+        <div className="navbar-start">
+          <div className='flex gap-2'>
+            <a href='/events' className="btn btn-ghost">Events</a>
+            <a href='/reservations' className="btn btn-ghost">Reservations</a>
+          </div>
+        </div>
+        <div className="navbar-end">
+          <div className="flex gap-2">
+            <a href='/profile' className='btn btn-ghost'>Profile</a>
+            <a href='/logout' className='btn btn-ghost'>Logout</a>
+          </div>
+        </div>
+      </div>
+
+<div className="card w-full max-w-md bg-base-100 shadow-md p-6 m-6">
+  <div className="card-body gap-4">
+      <h1 className='card-title'> {user.rank} {user.firstName} {user.lastName}</h1>
+     </div>
+      <div className="space-y-2">
+        <p><span className="font-semibold">Email</span>: {user.email}</p>
+        <p><span className="font-semibold">Phone</span>: {user.phone}</p>
+      </div>
+      <div>
+        <div className='card-actions jutify-end mt-2'>
+        <button type='submit' className='btn btn-primary'>Edit </button>
+      <button type='submit' className='btn btn-primary'>Update Password </button>
+      
+
+        </div>
+        </div>
+     
+      
+      </div></div>
+      
+    </>
+  )
+}
